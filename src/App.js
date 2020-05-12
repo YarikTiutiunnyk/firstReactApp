a572bf4ab27a4445d8f01b9a0000e31bfd4b9e74
YarikTiutiunnyk <50622079+YarikTiutiunnyk@users.noreply.github.com>
2020-05-12 20:59:04 +0300

    commit_1
    My first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+/*
+
+*/
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Srart!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
