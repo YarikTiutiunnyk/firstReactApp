12c8e292e592556724735224d8097426187d3316
YarikTiutiunnyk <50622079+YarikTiutiunnyk@users.noreply.github.com>
2020-05-24 22:45:23 +0300

    Updates

---
+++ precache-manifest.712a4777ede7391137d16f7e16f5616a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "647c1fd8740eeee6a8e288dec680e87a",
+    "url": "/firstReactApp/index.html"
+  },
+  {
+    "revision": "ff4a53ad010d50418059",
+    "url": "/firstReactApp/static/css/main.d57c5222.chunk.css"
+  },
+  {
+    "revision": "958630f7fac646e75b3a",
+    "url": "/firstReactApp/static/js/2.8d2bf017.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/firstReactApp/static/js/2.8d2bf017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff4a53ad010d50418059",
+    "url": "/firstReactApp/static/js/main.39b80b75.chunk.js"
+  },
+  {
+    "revision": "e75e16612ffe3d8b3204",
+    "url": "/firstReactApp/static/js/runtime-main.0efdb11b.js"
+  },
+  {
+    "revision": "8a67a3bc89059490f8a117cb7058c6fd",
+    "url": "/firstReactApp/static/media/404logolong.8a67a3bc.png"
+  },
+  {
+    "revision": "f12fd6532023d87060122548e8f7d754",
+    "url": "/firstReactApp/static/media/logoImg.f12fd653.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dc4545ead36555ab87cd8177cb9c27b2.js"
+  "/firstReactApp/precache-manifest.712a4777ede7391137d16f7e16f5616a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/firstReactApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
