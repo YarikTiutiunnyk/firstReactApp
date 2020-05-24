4f602be640d952d9a5febaac5ce95e0323c390b6
YarikTiutiunnyk <50622079+YarikTiutiunnyk@users.noreply.github.com>
2020-05-24 22:26:31 +0300

    Updates

---
+++ precache-manifest.dc4545ead36555ab87cd8177cb9c27b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9280dc469e104bd9262829212261d30d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fdd8ed9c6649f8e2b54",
+    "url": "/static/css/main.d57c5222.chunk.css"
+  },
+  {
+    "revision": "958630f7fac646e75b3a",
+    "url": "/static/js/2.8d2bf017.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.8d2bf017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fdd8ed9c6649f8e2b54",
+    "url": "/static/js/main.be6cdaa2.chunk.js"
+  },
+  {
+    "revision": "52ff117f383dd3ed2362",
+    "url": "/static/js/runtime-main.52922440.js"
+  },
+  {
+    "revision": "8a67a3bc89059490f8a117cb7058c6fd",
+    "url": "/static/media/404logolong.8a67a3bc.png"
+  },
+  {
+    "revision": "f12fd6532023d87060122548e8f7d754",
+    "url": "/static/media/logoImg.f12fd653.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/firstReactApp/precache-manifest.9603296427dfddba957d1091b4088aed.js"
+  "/precache-manifest.dc4545ead36555ab87cd8177cb9c27b2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/firstReactApp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
