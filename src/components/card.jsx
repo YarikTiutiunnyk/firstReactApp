38ef3ded0c9f23be98c73399b779109cdff4b505
YarikTiutiunnyk <50622079+YarikTiutiunnyk@users.noreply.github.com>
2020-08-20 13:46:33 +0300

    add placeholder in card component

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,34 +1,109 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Card, Icon, Image } from 'semantic-ui-react';
+import { Card, Icon, Image, Placeholder } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
 import logo404 from '../img/404logolong.png';
 
-const FilmCard = (props) => (
-  <Card>
+class FilmCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { imgIsloading: true };
+
+    this.handleImageLoaded = this.handleImageLoaded.bind(this);
+  }
+
+  handleImageLoaded() {
+    this.setState({ imgIsloading: false });
+    //console.log(`image loaded (${this.state.imgIsloading})`);
+  }
+
+  render() {
+    const { imgIsloading } = this.state;
+    const imageStyle = imgIsloading ? { display: 'none' } : {};
+    return (
+      <Card>
+        {imgIsloading && (
+          <Placeholder>
+            <Placeholder.Image square />
+          </Placeholder>
+        )}
+        <Image
+          src={this.props.Poster === 'N/A' ? logo404 : this.props.Poster}
+          wrapped
+          ui={false}
+          onLoad={this.handleImageLoaded}
+          style={imageStyle}
+        />
+
+        <Card.Content>
+          <Card.Header>
+            <Link to={`${this.props.currentURL}/${this.props.imdbID}`}>
+              {this.props.Title === 'N/A'
+                ? 'Film name not found'
+                : this.props.Title}
+            </Link>
+          </Card.Header>
+          <Card.Meta>
+            <span className="date">{`Published in ${this.props.Year}`}</span>
+          </Card.Meta>
+          <Card.Description></Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <Icon name="film" />
+          {` ${this.props.Type}`}
+        </Card.Content>
+      </Card>
+    );
+  }
+}
+
+/*function FilmCard2(props) {
+  const [imgIsLoaded, setImgIsLoaded] = useState(true);
+
+  function handleImageLoaded() {
+    setImgIsLoaded(!imgIsLoaded);
+    console.log(`image loaded (${imgIsLoaded})`);
+  }
+
+  let img1 = (
     <Image
       src={props.Poster === 'N/A' ? logo404 : props.Poster}
       wrapped
       ui={false}
+      onLoad={handleImageLoaded}
     />
-    <Card.Content>
-      <Card.Header>
-        <Link to={`${props.currentURL}/${props.imdbID}`}>
-          {props.Title === 'N/A' ? 'Film name not found' : props.Title}
-        </Link>
-      </Card.Header>
-      <Card.Meta>
-        <span className="date">{`Published in ${props.Year}`}</span>
-      </Card.Meta>
-      <Card.Description></Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <Icon name="film" />
-      {` ${props.Type}`}
-    </Card.Content>
-  </Card>
-);
+  );
+
+  return (
+    <Card>
+      {imgIsLoaded ? (
+        <Placeholder>
+          <Placeholder.Image square />
+        </Placeholder>
+      ) : (
+        img1
+      )}
+
+      <Card.Content>
+        <Card.Header>
+          <Link to={`${props.currentURL}/${props.imdbID}`}>
+            {props.Title === 'N/A' ? 'Film name not found' : props.Title}
+          </Link>
+        </Card.Header>
+        <Card.Meta>
+          <span className="date">{`Published in ${props.Year}`}</span>
+        </Card.Meta>
+        <Card.Description></Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Icon name="film" />
+        {` ${props.Type}`}
+      </Card.Content>
+    </Card>
+  );
+}
+);*/
 FilmCard.propTypes = {
   Poster: PropTypes.string.isRequired,
   imdbID: PropTypes.string.isRequired,
